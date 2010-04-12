73b1636046726d1c01a58675d20a6b669fd86fe1
richard.sabbarton <richard.sabbarton@2623e155-7324-2af9-a167-a3d837c1c561>
2010-04-12 20:36:14 +0000

    Setup initial rendering code that shows just a single quad with the texture loaded from a resource.

--- fireworks.cpp
+++ fireworks.cpp
@@ -1,1 +1,68 @@
 // fireworks.cpp
+#include "fireworks.h"
+
+
+
+GLuint txSparkle;
+
+
+
+bool LoadBitmapTexture(int RES, GLuint &texid)					// Creates Texture From A Bitmap File
+{
+	HBITMAP hBMP;														// Handle Of The Bitmap
+	BITMAP	BMP;														// Bitmap Structure
+
+	glGenTextures(1, &texid);   // Create The Texture
+
+	hBMP=(HBITMAP)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(RES), IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION );
+
+	if (!hBMP)	return FALSE;														// Does The Bitmap Exist?
+
+	GetObject(hBMP, sizeof(BMP), &BMP);									// Get The Object
+																		// hBMP:        Handle To Graphics Object
+																		// sizeof(BMP): Size Of Buffer For Object Information
+																		// &BMP:        Buffer For Object Information
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 4);								// Pixel Storage Mode (Word Alignment / 4 Bytes)
+
+	// Typical Texture Generation Using Data From The Bitmap
+	glBindTexture(GL_TEXTURE_2D, texid);								// Bind To The Texture ID
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	// Linear Min Filter
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	// Linear Mag Filter
+	glTexImage2D(GL_TEXTURE_2D, 0, 3, BMP.bmWidth, BMP.bmHeight, 0, GL_BGR_EXT, GL_UNSIGNED_BYTE, BMP.bmBits);
+	DeleteObject(hBMP);													// Delete The Object
+	return TRUE;														// Loading Was Successful
+}
+
+
+
+
+void InitFireworks(){
+     
+     LoadBitmapTexture(IDB_SPARKLE, txSparkle);
+     
+     
+     
+     
+}
+
+
+
+void RenderFireworks(){
+     
+     glTranslatef(0.0f, 0.0f, -5.0f);
+     glEnable(GL_TEXTURE_2D);
+     glBindTexture(GL_TEXTURE_2D, txSparkle);
+     
+     glBegin(GL_QUADS);
+     
+       glVertex3f(-1.0f, -1.0f, 0.0f); glTexCoord2f(0.0f, 0.0f);
+       glVertex3f( 1.0f, -1.0f, 0.0f); glTexCoord2f(1.0f, 0.0f);
+       glVertex3f( 1.0f,  1.0f, 0.0f); glTexCoord2f(1.0f, 1.0f);
+       glVertex3f(-1.0f,  1.0f, 0.0f); glTexCoord2f(0.0f, 1.0f);
+     
+     glEnd();
+     
+}
+
+
+
--- fireworks.h
+++ fireworks.h
@@ -1,5 +1,19 @@
 // Fireworks.h
-
+#include <windows.h>
 #include <gl\gl.h>
 #include <gl\glu.h>
 #include <gl\glext.h>
+
+#include "resource.h"
+
+
+extern GLuint txSparkle;
+
+
+bool LoadBitmapTexture(int RES, GLuint &texid);
+
+
+void InitFireworks();
+
+void RenderFireworks();
+
