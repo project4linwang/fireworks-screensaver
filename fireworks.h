c3de996617c4985051e3443ddef73078971ea816
richard.sabbarton <richard.sabbarton@2623e155-7324-2af9-a167-a3d837c1c561>
2010-04-12 22:47:13 +0000

    Making progress with the particle system setup.

--- fireworks.h
+++ fireworks.h
@@ -6,14 +6,26 @@
 
 #include "resource.h"
 
+#define MAX_FIREWORKS 3
+#define MAX_BANGPARTICLES 100
 
 extern GLuint txSparkle;
 
+struct PARTICLE {
+       
+    float x,y,z;
+    float sx,sy,sz;
+    float tsx,tsy,tsz;
+    float R,G,B;
+    int TTL;       
+       
+};
+
+extern PARTICLE BangParticles[MAX_BANGPARTICLES][MAX_FIREWORKS];
+
+extern int FireworkCount;
 
 bool LoadBitmapTexture(int RES, GLuint &texid);
-
-
 void InitFireworks();
-
 void RenderFireworks();
-
+void CreateNewBang();
